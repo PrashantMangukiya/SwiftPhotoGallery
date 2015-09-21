b6cd59e1fd9957dda7e849c2c68cfecb2a553f52
PrashantKumar Mangukiya <pbMangukiya@gmail.com>
2015-09-21 10:07:45 +0530

    Added source code Swift 2, Xcode 7, iOS 9
    Added source code that was build using Swift 2, Xcode 7, iOS 9.

---
+++ SwiftPhotoGallery/SwiftPhotoGallery/Common.swift
@@ -0,0 +1,13 @@
+//
+//  Common.swift
+//  SwiftPhotoGallery
+//
+//  Created by Prashant on 13/09/15.
+//  Copyright (c) 2015 PrashantKumar Mangukiya. All rights reserved.
+//
+
+import Foundation
+
+
+// Remote data folder path
+var REMOTE_DATA_FOLDER_PATH = "http://download.prashantmangukiya.com/SwiftPhotoGallery-Data/"
